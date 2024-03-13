076dac556518fbbbb3b950073578003c64d1332e
Pedro Park <pedro.park@ael.com.br>
2024-03-13 16:37:57 -0300

    Exercicio 3 - SUV

--- src/App.java
+++ src/App.java
@@ -1,18 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Carro Utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        Carro SUV = new Carro("SUV", TipoCombustivel.GASOLINA, 8, 55);
 
         System.out.println("Tipos de veiculos:");
-        System.out.println(Utilitario);
+        System.out.println(SUV);
 
         System.out.println("\n\n----------------");
-        System.out.println("\n Abastencendo carro "+ Utilitario.getModelo() +" com "+ TipoCombustivel.DIESEL.toString());
-        Utilitario.abastece(TipoCombustivel.DIESEL,70);
-        System.out.println(Utilitario);
-        System.out.println("\n Viajando com o carro "+ Utilitario.getModelo());
-        Utilitario.viaja(250);
-        Utilitario.viaja(150);
-        System.out.println(Utilitario);
+        System.out.println("\n Abastencendo carro "+ SUV.getModelo() +" com "+ TipoCombustivel.GASOLINA.toString());
+        SUV.abastece(TipoCombustivel.GASOLINA,55);
+        System.out.println(SUV);
+        System.out.println("\n Viajando com o carro "+ SUV.getModelo());
+        SUV.viaja(250);
+        SUV.viaja(150);
+        System.out.println(SUV);
     }
 }
